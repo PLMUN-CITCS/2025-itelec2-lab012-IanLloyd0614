5cedaf61596d49a1fde94d156c4e56cf2f2e3884
unknown <angelesianlloyd_bist@gmail.com>
2025-02-15 13:49:07 +0800

    Submitting Python Week 04 - Laboratory # 12

---
+++ switch_ternary_operator.py
@@ -0,0 +1,24 @@
+# Ian Lloyd Santiago Angeles
+# ITELEC2
+# Laboratory #12 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+def main():
+
+    day_messages = {
+    "monday": "Today is Monday.",
+    "tuesday": "Today is Tuesday.",
+    "wednesday": "Today is Wednesday.",
+    "thursday": "Today is Thursday.",
+    "friday": "Today is Friday.",
+    "saturday": "Today is Saturday.",
+    "sunday": "Today is Sunday."
+    }
+    day = input("Enter a day of the week: ").strip().lower()
+    message = day_messages.get(day, "Invalid day entered.")
+    day_type = "Weekend" if day in ("saturday", "sunday") else "Weekday" if day in day_messages else "unknown"
+    print(message)
+    print("It's a", day_type + "!")
+
+if __name__ == "__main__":
+    main()
